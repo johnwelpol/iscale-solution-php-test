8939ec25402c5ad7f89010ff6ae6c6685b516440
Johnwel <johnwellibariospol@gmail.com>
2024-10-29 01:38:14 +0800

    initialize base on the default connection.

--- app/Repository/BaseRepository.php
+++ app/Repository/BaseRepository.php
@@ -6,6 +6,11 @@ use App\Database\Contracts\DBInterface;
 abstract class BaseRepository {
     protected DBInterface $db;
 
+    public function __construct()
+    {   
+        $this->connection(app()->getDBManager()->getDefaultConnection());
+    }
+
     public function connection(string $connection) {
         $this->db = app()->getDBManager()->connection($connection);
     }
