dc8023ae57a9226cdee29d5aed8aa55d35e0a37b
Johnwel <johnwellibariospol@gmail.com>
2024-10-29 01:38:32 +0800

    Lazy loading improvement.

--- index.php
+++ index.php
@@ -4,18 +4,17 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/app/helpers.php';
 
 define('BASE_DIR', __DIR__);
+use App\Manager\DBManager;
 use App\Manager\AppManager;
 use App\Manager\ConfigManager;
-use App\Manager\DBManager;
 use App\Repository\NewsRepository;
 use App\Repository\CommentRepository;
 
-$config = new ConfigManager();
-$dbManager = new DBManager();
-$app = AppManager::getInstance();
-$app->setConfigInstance($config)
-	->setDBManagerInstance($dbManager)
-	->boot();
+$config = ConfigManager::getInstance();
+$dbManager = DBManager::getInstance();
+$app = AppManager::getInstance()
+	->setConfigInstance($config)
+	->setDBManagerInstance($dbManager);
 
 foreach (NewsRepository::getInstance()->listNews() as $news) {
 	echo("############ NEWS " . $news->getTitle() . " ############\n");
